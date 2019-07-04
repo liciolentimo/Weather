80ca4aad6afc746886c30ad69aa16b14dae23bb4
liciolentimo <lentimo@gmail.com>
2019-07-05 00:19:15 +0300

    Added current location string

--- app/src/main/java/com/lentimosystems/licio/weather/Constants/Constants.java
+++ app/src/main/java/com/lentimosystems/licio/weather/Constants/Constants.java
@@ -1,5 +1,8 @@
 package com.lentimosystems.licio.weather.Constants;
 
+import android.location.Location;
+
 public class Constants {
     public static final String APP_ID = "492160633aea4cb7334b9ef4c3cef64c";
+    public static Location currentLocation = null;
 }
