fb993e841b259149710bca0036e87e299c55b395
liciolentimo <lentimo@gmail.com>
2019-07-07 15:39:48 +0300

    Added static methods to display time

--- app/src/main/java/com/lentimosystems/licio/weather/Constants/Constants.java
+++ app/src/main/java/com/lentimosystems/licio/weather/Constants/Constants.java
@@ -2,7 +2,22 @@ package com.lentimosystems.licio.weather.Constants;
 
 import android.location.Location;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Constants {
     public static final String APP_ID = "492160633aea4cb7334b9ef4c3cef64c";
     public static Location currentLocation = null;
+    public static String convertUnixToDate(long dt){
+        Date date = new Date(dt*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm EEE MM yyyy");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
+    public static String convertUnixToHour(long dt){
+        Date date = new Date(dt*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
 }
